aab1fb68c1bfc53f2d5d5aaf608c24cb4eadf6ed
roberto.desalvo <roberto.desalvo@outlook.com>
2026-04-30 16:28:46 +0000

    Remove login page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,37 +6,36 @@ import LandingPage from "./pages/LandingPage";
 import TransactionsPage from "./pages/TransactionsPage";
 import CategoriesPage from "./pages/CategoriesPage";
 import AccountsPage from "./pages/AccountsPage";
-import LoginPage from "./pages/LoginPage";
-import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
+import { MsalAuthenticationTemplate } from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
+import { loginRequest } from "./config/authConfig";
 
 function App() {
   return (
     <BrowserRouter>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-          width: "100%",
-          bgcolor: "background.default",
-          overflowX: "hidden",
-        }}
+      <MsalAuthenticationTemplate
+        interactionType={InteractionType.Redirect}
+        authenticationRequest={loginRequest}
       >
-        <HomeHeader />
-
-        <AuthenticatedTemplate>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+            width: "100%",
+            bgcolor: "background.default",
+            overflowX: "hidden",
+          }}
+        >
+          <HomeHeader />
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/transazioni" element={<TransactionsPage />} />
             <Route path="/categorie" element={<CategoriesPage />} />
             <Route path="/account" element={<AccountsPage />} />
           </Routes>
-        </AuthenticatedTemplate>
-
-        <UnauthenticatedTemplate>
-          <LoginPage />
-        </UnauthenticatedTemplate>
-      </Box>
+        </Box>
+      </MsalAuthenticationTemplate>
     </BrowserRouter>
   );
 }
